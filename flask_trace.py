c59b8364230a5d9597c46c2420ae65ca672cbbca
ethomatos <et@thomatos.org>
2022-02-02 11:11:38 -0500

    Lots of modifications with success in using secrets.

--- flask_trace.py
+++ flask_trace.py
@@ -1,10 +1,38 @@
 from flask import Flask
+import mysql.connector
+import os
+
+# Get environment variables
+USER = os.getenv('MYSQL_USER')
+PASSWORD = os.environ.get('MYSQL_PASSWORD')
+DB = os.environ.get('MYSQL_DATABASE')
+
+def read_password():
+	password = ""
+	with open(PASSWORD, 'r') as psw:
+		for line in psw:
+			password = line.strip()
+	return password
+
+def mysql_conn(user, password, db):
+	host = 'mysql'
+	cnx = mysql.connector.connect(user=user, password=password, host=host, database=db)
+	return cnx;
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is Flask running in a Docker container.'
-
+	cnx = mysql_conn(USER, read_password(), DB)
+	cursor = cnx.cursor()
+	query = ("show tables")
+	cursor.execute(query)
+	count = 0
+	for table in cursor:
+		count += 1	
+	return 'This is Flask running in a Docker container querying a MySQL container with ' + str(count) + ' tables'
+	cursor.close()
+	cnx.close()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
